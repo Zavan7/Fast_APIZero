5c7c3434d09ac4c8a1ed42b20a037dc1fb7bb4a3
Vitor Zavan <vitorzavanj@gmail.com>
2025-05-18 23:23:02 -0300

    atualização

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,11 +2,18 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='FastAPI Bolada')
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/user/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
